3e4c2b604a544c79d48970c7f08b6c6236acd70c
robb <364886923@qq.com>
2019-07-18 15:38:49 +0800

    Update StringUtil.h

--- code/StringUtil.h
+++ code/StringUtil.h
@@ -1,29 +1,26 @@
 #pragma once
 
-
 #ifndef _STRING_UTIL_H_
 #define _STRING_UTIL_H_
 
 
 #include <string>
 
-
+// 
 class CStringUtil
 {
 public:
-    CStringUtil();
-    ~CStringUtil();
-
-public:
-    static std::string  WCharToChar(const wchar_t *pwszSrc);
-    static std::string  WCharToChar(std::wstring& wszSrc);
-    static std::string  WCharToUTF8(const wchar_t *pwszSrc);
-    static std::string  WCharToUTF8(std::wstring& wszSrc);
     static std::wstring CharToWChar(const char *pszSrc);
-    static std::wstring CharToWChar(std::string& szSrc);
-    static std::wstring UTF8ToWChar(const char *pszSrc);
-    static std::wstring UTF8ToWChar(std::string& szSrc);
-};
+    static std::wstring CharToWChar(const std::string& szSrc);
+    static std::wstring UTF8ToWChar(const char* pszSrc);
+    static std::wstring UTF8ToWChar(const std::string& szSrc);
+    static std::string  WCharToChar(const wchar_t *pwszSrc);
+    static std::string  WCharToChar(const std::wstring& wszSrc);
+    static std::string  WCharToUTF8(const wchar_t *pwszSrc);
+    static std::string  WCharToUTF8(const std::wstring& wszSrc);
 
+    CStringUtil() = delete;
+    ~CStringUtil() = delete;
+};
 
 #endif // _STRING_UTIL_H_
