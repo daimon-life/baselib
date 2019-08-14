8eb6d26ae0a8930f95377fbac36629392f45409a
robb_win <364886923@qq.com>
2019-08-14 22:37:50 +0800

    调整目录结构-baseutil

--- include/version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define FILE_VERSION                    1, 0, 0, 0
-#define FILE_VERSION_STRING_T1(ver)     #ver
-#define FILE_VERSION_STRING_T2(ver)     FILE_VERSION_STRING_T1(ver)
-#define FILE_VERSION_STRING             FILE_VERSION_STRING_T2(FILE_VERSION)
--- test/test_dylibutil.cpp
+++
@@ -1,7 +0,0 @@
-#include "DylibUtil.h"
-
-int main(int argc, char **argv)
-{
-	void* hModule = CDylibUtil::LoadDylib("base.dll");
-	return 0;
-}
